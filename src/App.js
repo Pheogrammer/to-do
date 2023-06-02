da7306b35352c5c192ced3effc6cb148fcae7d12
Alfeo Raymond <ngelime07@gmail.com>
2023-06-02 22:07:41 +0300

    i have added navigations, every more js is inside the components folder and the API connections inside ENV.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import Todos from './components/Todos';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const username = process.env.REACT_APP_USERNAME;
+        const password = process.env.REACT_APP_PASSWORD;
+        const name = process.env.REACT_APP_NAME;
+
+        const response = await axios.get(
+          `https://dev.hisptz.com/dhis2/api/dataStore/${name}?fields=.&page=1&page=2`,
+          {
+            auth: {
+              username,
+              password,
+            },
+          }
+        );
+        setTodos(response.data.entries);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is a test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todos" element={<Todos todos={todos} />} />
+        </Routes>
+      </div>
+    </Router>
   );
+  
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Todos({ todos }) {
+  return (
+    <div>
+      <h1>Todos</h1>
+      {/* Display todos */}
+    </div>
+  );
+}
+
+export default Todos;
