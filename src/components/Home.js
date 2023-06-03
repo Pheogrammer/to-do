9f35f8e346239e7872e60269d3c0247d22c1ac9d
Alfeo Raymond <ngelime07@gmail.com>
2023-06-03 17:12:56 +0300

    i have splited the done and undone tasks

--- src/components/Home.js
+++ src/components/Home.js
@@ -227,13 +227,52 @@ function Home() {
       }
     }
   };
-
-  const generateTableRows = () => {
-    const sortedItems = allTodoItems.sort((a, b) => {
-      const dateA = new Date(a.value.created);
-      const dateB = new Date(b.value.created);
-      return dateA - dateB;
+  const generateCompletedTasks = () => {
+    const sortedCompletedItems = allTodoItems
+      .filter(item => item.value.completed) // Filter items with completed status set to true
+      .sort((a, b) => {
+        const dateA = new Date(a.value.created);
+        const dateB = new Date(b.value.created);
+        return dateA - dateB;
+      });
+  
+    return sortedCompletedItems.map((item, index) => {
+      const createdDate = new Date(item.value.created);
+      const currentDate = new Date();
+      const timeDiff = Math.abs(currentDate - createdDate);
+      const daysPassed = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+  
+      const formattedDate = createdDate.toLocaleString('en-GB', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+  
+      return (
+        <tr key={item.id}>
+          <td>{index + 1}</td>
+          <td>{item.value.title}</td>
+          <td>{formattedDate}</td>
+          <td>{item.value.completed ? 'Finished' : 'Pending'}</td>
+          <td>{daysPassed}</td>
+          <td>
+            <Button onClick={() => handleModalOpen(item)}>Details</Button>
+          </td>
+        </tr>
+      );
     });
+  };
+  
+  const generateTableRows = () => {
+    const sortedItems = allTodoItems
+      .filter(item => !item.value.completed) 
+      .sort((a, b) => {
+        const dateA = new Date(a.value.created);
+        const dateB = new Date(b.value.created);
+        return dateA - dateB;
+      });
 
     return sortedItems.map((item, index) => {
       const createdDate = new Date(item.value.created);
@@ -252,7 +291,7 @@ function Home() {
       return (
         <tr key={item.id}>
           <td>{index + 1}</td>
-          <td >{item.value.title}</td>
+          <td>{item.value.title}</td>
           <td>{formattedDate}</td>
           <td>{item.value.completed ? 'Finished' : 'Pending'}</td>
           <td>{daysPassed}</td>
@@ -263,6 +302,7 @@ function Home() {
       );
     });
   };
+
 
 
   return (
@@ -298,8 +338,10 @@ function Home() {
             <div className="card">
               <div className="text-center mt-3">
                 {allTodoItems.length > 0 ? (
-                  <div className="card-head row">
-                    <div className="col"></div>
+                  <div className="card-head row ">
+                    <div className="col">
+                      <h4>Pending Todos</h4>
+                    </div>
                     <div className="col-md-4">
                       <Button
                         variant="success"
@@ -308,10 +350,12 @@ function Home() {
                         Add New Todo Task
                       </Button>
                     </div>
+                    
                   </div>
                 ) : (
                   <div>
                     <p>No todo items due today.</p>
+
                     <div className="col-md-4">
                       <Button variant="primary" onClick={handleAddModalOpen}>
                         Add Todo
@@ -457,18 +501,13 @@ function Home() {
         </Modal.Footer>
       </Modal>
       <div className="container">
+
         <div className="row mt-5">
           <div className="col">
             <div className="card">
-              <div className="card-body text-center">
-                Completed Todos
+              <div className="card-head text-center mt-3">
+                <h4>Completed Todos</h4>
               </div>
-            </div>
-          </div>
-        </div>
-        <div className="row mt-5">
-          <div className="col">
-            <div className="card">
               <div className="card-body text-center">
                 <table className="table">
                   <thead>
@@ -483,14 +522,9 @@ function Home() {
                   </thead>
 
                   <tbody>
-                    <tr>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
+                    
+                     {generateCompletedTasks()}
+                   
                   </tbody>
                 </table>
               </div>
